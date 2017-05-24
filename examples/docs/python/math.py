24b15d03fcc310f54876f022af0d0a29bf61352d
Miguel Molina <hi@mvader.me>
2017-05-24 12:59:05 +0200

    include example of python documentation

---
+++ examples/docs/python/math.py
@@ -0,0 +1,41 @@
+def sum(a, b):
+  """
+  Sums two integers.
+
+  `sum` takes an integer a and an integer b and sums them!
+
+  :param a: First number to sum.
+  :param b: Second number to sum.
+  :type a: int
+  :type b: int
+  :returns: the sum of both numbers!
+  :rtype: int
+
+  :Example:
+
+  >>> sum(1, 5)
+  6
+  """
+  return a + b
+
+
+def sub(a, b):
+  """
+  Subtracts two integers.
+
+  `sub` takes an integer a and an integer b and subtracts b from a!
+
+  :param a: Number to subtract from.
+  :param b: Number to subtract from `a`.
+  :type a: int
+  :type b: int
+  :returns: a - b
+  :rtype: int
+
+  :Example:
+
+  >>> sub(5, 1)
+  4
+  """
+  return a - b
+
